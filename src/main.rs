eb90065b88b8a2f8e2fca8bc94cbdbc689c267b4
Robert Helmer <rhelmer@rhelmer.org>
2019-03-28 18:40:08 -0700

    stop using explicit paths in test code

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,32 @@ mod prio;
 
 use prio::client;
 use prio::config;
-use prio::encrypt;
-use prio::server;
+use prio::encrypt::{PrivateKey, PrivateKey_clear, PublicKey, PublicKey_clear};
+use prio::server::{
+    PrioServer, PrioServer_aggregate, PrioServer_clear, PrioServer_new, PrioVerifier_set_data,
+    PrioPacketVerify1, PrioPacketVerify1_clear, PrioPacketVerify1_new, PrioPacketVerify1_set_data,
+    PrioPacketVerify2, PrioPacketVerify2_clear, PrioPacketVerify2_new, PrioPacketVerify2_set_data,
+    PrioTotalShare_final, PrioVerifier, PrioVerifier_clear, PrioVerifier_new, PrioVerifier_isValid,
+    PrioTotalShare, PrioTotalShare_clear, PrioTotalShare_new, PrioTotalShare_set_data
+};
 
 fn main() {
     println!("Setting up...");
-    let mut pkA: prio::encrypt::PublicKey = 0 as prio::encrypt::PublicKey;
-    let mut pkB: prio::encrypt::PublicKey = 0 as prio::encrypt::PublicKey;
-    let mut skA: prio::encrypt::PrivateKey = 0 as prio::encrypt::PrivateKey;
-    let mut skB: prio::encrypt::PrivateKey = 0 as prio::encrypt::PrivateKey;
+    let mut pkA: PublicKey = 0 as PublicKey;
+    let mut pkB: PublicKey = 0 as PublicKey;
+    let mut skA: PrivateKey = 0 as PrivateKey;
+    let mut skB: PrivateKey = 0 as PrivateKey;
 
-    let mut sA: prio::server::PrioServer = 0 as prio::server::PrioServer;
-    let mut sB: prio::server::PrioServer = 0 as prio::server::PrioServer;
-    let mut vA: prio::server::PrioVerifier = 0 as prio::server::PrioVerifier;
-    let mut vB: prio::server::PrioVerifier = 0 as prio::server::PrioVerifier;
-    let mut p1A: prio::server::PrioPacketVerify1 = 0 as prio::server::PrioPacketVerify1;
-    let mut p1B: prio::server::PrioPacketVerify1 = 0 as prio::server::PrioPacketVerify1;
-    let mut p2A: prio::server::PrioPacketVerify2 = 0 as prio::server::PrioPacketVerify2;
-    let mut p2B: prio::server::PrioPacketVerify2 = 0 as prio::server::PrioPacketVerify2;
-    let mut tA: prio::server::PrioTotalShare = 0 as prio::server::PrioTotalShare;
-    let mut tB: prio::server::PrioTotalShare = 0 as prio::server::PrioTotalShare;
+    let mut sA: PrioServer = 0 as PrioServer;
+    let mut sB: PrioServer = 0 as PrioServer;
+    let mut vA: PrioVerifier = 0 as PrioVerifier;
+    let mut vB: PrioVerifier = 0 as PrioVerifier;
+    let mut p1A: PrioPacketVerify1 = 0 as PrioPacketVerify1;
+    let mut p1B: PrioPacketVerify1 = 0 as PrioPacketVerify1;
+    let mut p2A: PrioPacketVerify2 = 0 as PrioPacketVerify2;
+    let mut p2B: PrioPacketVerify2 = 0 as PrioPacketVerify2;
+    let mut tA: PrioTotalShare = 0 as PrioTotalShare;
+    let mut tB: PrioTotalShare = 0 as PrioTotalShare;
 
     unsafe {
         prio::encrypt::Keypair_new(&mut skA, &mut pkA);;
@@ -44,25 +50,25 @@ fn main() {
         //   * Server B only comes online when the two servers want to compute
         //     the final aggregate statistics.
         println!("Initializing server objects...");
-        sA = prio::server::PrioServer_new(cfg, prio::client::PRIO_SERVER_A, skA, serverSecret);
-        sB = prio::server::PrioServer_new(cfg, prio::client::PRIO_SERVER_B, skB, serverSecret);
+        sA = PrioServer_new(cfg, prio::client::PRIO_SERVER_A, skA, serverSecret);
+        sB = PrioServer_new(cfg, prio::client::PRIO_SERVER_B, skB, serverSecret);
 
         // Initialize empty verifier objects
         println!("Initializing verifier objects...");
-        vA = prio::server::PrioVerifier_new(sA);
-        vB = prio::server::PrioVerifier_new(sB);
+        vA = PrioVerifier_new(sA);
+        vB = PrioVerifier_new(sB);
 
         // Initialize shares of final aggregate statistics
         println!("Initializing of final aggregate statistics...");
-        tA = prio::server::PrioTotalShare_new();
-        tB = prio::server::PrioTotalShare_new();
+        tA = PrioTotalShare_new();
+        tB = PrioTotalShare_new();
 
         // Initialize shares of verification packets
         println!("Initializing shares of verification packets...");
-        p1A = prio::server::PrioPacketVerify1_new();
-        p1B = prio::server::PrioPacketVerify1_new();
-        p2A = prio::server::PrioPacketVerify2_new();
-        p2B = prio::server::PrioPacketVerify2_new();
+        p1A = PrioPacketVerify1_new();
+        p1B = PrioPacketVerify1_new();
+        p2A = PrioPacketVerify2_new();
+        p2B = PrioPacketVerify2_new();
 
         // I. CLIENT DATA SUBMISSION.
         // Initialize empty client result objects
@@ -108,30 +114,30 @@ fn main() {
         // from its peer.
 
         // Set up a Prio verifier object.
-        prio::server::PrioVerifier_set_data(vA, forServerA, aLen);
-        prio::server::PrioVerifier_set_data(vB, forServerB, bLen);
+        PrioVerifier_set_data(vA, forServerA, aLen);
+        PrioVerifier_set_data(vB, forServerB, bLen);
 
         // Both servers produce a packet1. Server A sends p1A to Server B
         // and vice versa.
-        prio::server::PrioPacketVerify1_set_data(p1A, vA);
-        prio::server::PrioPacketVerify1_set_data(p1B, vB);
+        PrioPacketVerify1_set_data(p1A, vA);
+        PrioPacketVerify1_set_data(p1B, vB);
 
         // Both servers produce a packet2. Server A sends p2A to Server B
         // and vice versa.
-        prio::server::PrioPacketVerify2_set_data(p2A, vA, p1A, p1B);
-        prio::server::PrioPacketVerify2_set_data(p2B, vB, p1A, p1B);
+        PrioPacketVerify2_set_data(p2A, vA, p1A, p1B);
+        PrioPacketVerify2_set_data(p2B, vB, p1A, p1B);
 
         // Using p2A and p2B, the servers can determine whether the request
         // is valid. (In fact, only Server A needs to perform this
         // check, since Server A can just tell Server B whether the check
         // succeeded or failed.)
-        prio::server::PrioVerifier_isValid(vA, p2A, p2B);
-        prio::server::PrioVerifier_isValid(vB, p2A, p2B);
+        PrioVerifier_isValid(vA, p2A, p2B);
+        PrioVerifier_isValid(vB, p2A, p2B);
 
         // If we get here, the client packet is valid, so add it to the aggregate
         // statistic counter for both servers.
-        prio::server::PrioServer_aggregate(sA, vA);
-        prio::server::PrioServer_aggregate(sB, vB);
+        PrioServer_aggregate(sA, vA);
+        PrioServer_aggregate(sB, vB);
 
         // The servers repeat the steps above for each client submission.
 
@@ -141,39 +147,39 @@ fn main() {
         // their shares of the aggregate statistics.
         //
         // Server B can send tB to Server A.
-        prio::server::PrioTotalShare_set_data(tA, sA);
-        prio::server::PrioTotalShare_set_data(tB, sB);
+        PrioTotalShare_set_data(tA, sA);
+        PrioTotalShare_set_data(tB, sB);
 
         // Once Server A has tA and tB, it can learn the aggregate statistics
         // in the clear.
-        prio::server::PrioTotalShare_final(cfg, output, tA, tB);
+        PrioTotalShare_final(cfg, output, tA, tB);
 
 /*
         for (int i = 0; i < ndata; i++) {
             ASSERT_TRUE(output[i] == dataItems[i]);
         }
 */
-        prio::server::PrioTotalShare_clear(tA);
-        prio::server::PrioTotalShare_clear(tB);
+        PrioTotalShare_clear(tA);
+        PrioTotalShare_clear(tB);
 
-        prio::server::PrioPacketVerify2_clear(p2A);
-        prio::server::PrioPacketVerify2_clear(p2B);
+        PrioPacketVerify2_clear(p2A);
+        PrioPacketVerify2_clear(p2B);
 
-        prio::server::PrioPacketVerify1_clear(p1A);
-        prio::server::PrioPacketVerify1_clear(p1B);
+        PrioPacketVerify1_clear(p1A);
+        PrioPacketVerify1_clear(p1B);
 
-        prio::server::PrioVerifier_clear(vA);
-        prio::server::PrioVerifier_clear(vB);
+        PrioVerifier_clear(vA);
+        PrioVerifier_clear(vB);
 
-        prio::server::PrioServer_clear(sA);
-        prio::server::PrioServer_clear(sB);
+        PrioServer_clear(sA);
+        PrioServer_clear(sB);
         prio::config::PrioConfig_clear(cfg);
 
-        prio::encrypt::PublicKey_clear(pkA);
-        prio::encrypt::PublicKey_clear(pkB);
+        PublicKey_clear(pkA);
+        PublicKey_clear(pkB);
 
-        prio::encrypt::PrivateKey_clear(skA);
-        prio::encrypt::PrivateKey_clear(skB);
+        PrivateKey_clear(skA);
+        PrivateKey_clear(skB);
 
         prio::config::Prio_clear();
     }
